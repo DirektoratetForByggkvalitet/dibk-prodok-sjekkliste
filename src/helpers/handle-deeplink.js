037421231548cad73e19bbd7a95f3adde76a7e80
Kristoffer Brabrand <kristoffer@brabrand.no>
2019-02-04 12:38:00 +0100

    Set  prop to allow showing stuff only for those who entered from link

--- src/helpers/handle-deeplink.js
+++ src/helpers/handle-deeplink.js
@@ -1,5 +1,5 @@
 import { state, dsl } from 'losen';
-import find from 'lodash.find';
+import get from 'lodash.get';
 
 import schema from '../api/prodok-sjekkliste.json';
 
@@ -7,47 +7,51 @@ import schema from '../api/prodok-sjekkliste.json';
  * Redux middleware for getting the produktType from the hash and
  */
 export default function handleDeeplink({ getState }) {
-    return next => action => {
-        if (action.type !== 'persist/REHYDRATE') {
-            return next(action);
-        }
-
-        const [, produktType] = (window.location.hash.match(/^#produktType:(\w+)/) || [])
-
-        // Do nothing unless we have a produktType in the hash
-        if (!produktType) {
-            return next(action);
-        }
-
-        // Calculate the ceValgfri based on the state + specified produktType
-        const ceValgfri = dsl.default(schema.computed.ceValgfri)({
-            ...action.payload,
-            produktType
-        }).valid
-
-        return next({
-            ...action,
-            payload: {
-                ...action.payload || {},
-                [state.NAME]: {
-                    $computed: {
-                        ...action.payload[state.NAME].$computed || {},
-
-                        // override ceValgfri
-                        ceValgfri
-                    },
-
-                    // set produktType
-                    produktType,
-
-                    // set page based on ceValgfri and produktType
-                    page: (
-                        !ceValgfri
-                        ? 'ytelseserklæring'
-                        : 'byggevareOgProdukter'
-                    )
-                }
-            }
-        });
+  return next => action => {
+    if (action.type !== 'persist/REHYDRATE') {
+      return next(action);
     }
+
+    const [, produktType] = (window.location.hash.match(/^#produktType:(\w+)/) || [])
+
+    // Do nothing unless we have a produktType in the hash
+    if (!produktType) {
+      return next(action);
+    }
+
+    // Calculate the ceValgfri based on the state + specified produktType
+    const ceValgfri = dsl.default(schema.computed.ceValgfri)({
+      ...action.payload,
+      produktType
+    }).valid
+
+    return next({
+      ...action,
+      payload: {
+        ...action.payload || {},
+        [state.NAME]: {
+          $computed: {
+            ...get(action, ['payload', state.NAME, '$computed'], {}),
+
+            // override ceValgfri
+            ceValgfri,
+          },
+
+          // set produktType
+          produktType,
+
+          $external: {
+            enteredFromMagicLink: true,
+          },
+
+          // set page based on ceValgfri and produktType
+          page: (
+            !ceValgfri
+            ? 'ytelseserklæring'
+            : 'byggevareOgProdukter'
+          )
+        }
+      }
+    });
+  }
 }
