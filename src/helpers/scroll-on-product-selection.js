b1cb69907d1dd3496cb2d83abddd1504e5ebd4fc
tommyno <tommy@netliferesearch.com>
2019-02-05 09:46:33 +0100

    Fix scroll to next button by adding a delay

--- src/helpers/scroll-on-product-selection.js
+++ src/helpers/scroll-on-product-selection.js
@@ -1,15 +1,25 @@
 import zenscroll from 'zenscroll';
 
-document.querySelector('body').addEventListener('click', function(evt) {
-  // do nothing if the clicked element is not a radio button
-  if (!evt.target.type === 'radio') { return; }
+document.querySelector('body').addEventListener(
+  'click',
+  function(evt) {
+    // do nothing if the clicked element is not a radio button
+    if (!evt.target.type === 'radio') {
+      return;
+    }
 
-  // ...with the name: produktType
-  if (evt.target.name !== 'produktType') { return; }
+    // ...with the name: produktType
+    if (evt.target.name !== 'produktType') {
+      return;
+    }
 
-  // get the next-button
-  const nextButton = document.querySelector('#main main:last-child button');
-
-  // scoll to the next button
-  nextButton && zenscroll.intoView(nextButton, 200)
-}, true);
+    // need a small delay to ensure the next-button has been generated
+    setTimeout(() => {
+      // get the next-button
+      const nextButton = document.querySelector('#main main:last-child button');
+      // scoll to the next button
+      nextButton && zenscroll.intoView(nextButton, 200);
+    }, 100);
+  },
+  true
+);
