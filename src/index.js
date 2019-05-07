49ea19d76191ba7265d648d7aa44b1d1aa9bce00
Kristoffer Brabrand <kristoffer@brabrand.no>
2019-05-07 15:12:05 +0200

    Add multilingual support using the new translation logic in losen

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import translations from './api/translations';
 
 import './helpers/scroll-on-product-selection';
 
 const root = document.querySelector('div[data-bind], #root');
-let translations = JSON.parse(root.getAttribute('data-bind') || '{}');
-
-translations = Object.keys(translations).reduce((res, id) => {
-	const { title: heading, ...rest } = translations[id];
-
-	return {
-		...res,
-		[id]: {
-			heading,
-			...rest,
-		},
-	};
-}, {});
 
 ReactDOM.render(<App translations={translations} />, root); /* eslint no-undef: 0 */
